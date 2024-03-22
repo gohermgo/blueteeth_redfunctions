70d064dfc768356c21d30fec7854669c564a5532
Herman Gohn <gohnherman@gmail.com>
2024-03-22 23:45:03 +0100

    search device by id yuhu

--- src/lib.rs
+++ src/lib.rs
@@ -81,6 +81,42 @@ impl Drop for DeviceSearcher {
 type StatusVerifier = Box<dyn Fn(bool) + Send>;
 type BLStatusHandler = TypedEventHandler<BluetoothLEDevice, IInspectable>;
 impl DeviceSearcher {
+    const INVOKE: for<'a, 'b> fn(
+        &'a Option<BluetoothLEDevice>,
+        &'b Option<IInspectable>,
+    ) -> windows::core::Result<()> =
+        move |devopt: &Option<BluetoothLEDevice>, _args: &Option<IInspectable>| {
+            if let Some(device) = devopt {
+                let connected: bool = device.ConnectionStatus().ok().map_or(false, |status| {
+                    status == BluetoothConnectionStatus::Connected
+                });
+                println!("Connected: {}", connected);
+            };
+            Ok(())
+        };
+    async fn get_device_by_id(deviceid: &HSTRING) -> Option<DeviceWrapper> {
+        let Ok(device_future) = BluetoothLEDevice::FromIdAsync(deviceid) else {
+            return None;
+        };
+        let Ok(device) = device_future.await else {
+            return None;
+        };
+        // let handler: BLStatusHandler = TypedEventHandler::new(move |devopt: &Option<BluetoothLEDevice>, _args &Option<IInspectable>| {
+        //     if let Some(device) = devopt {
+        //         let connected: bool = device.ConnectionStatus().ok
+        //     };
+        //     Ok(())
+        // });
+        let handler = TypedEventHandler::new(Self::INVOKE);
+        let Ok(token) = device.ConnectionStatusChanged(&handler) else {
+            return None;
+        };
+        Some(DeviceWrapper {
+            device,
+            token,
+            services: vec![],
+        })
+    }
     /// Helper which makes the code smol
     async fn get_device_by_address(bluetoothaddress: u64) -> Option<DeviceWrapper> {
         let Ok(device_future) = BluetoothLEDevice::FromBluetoothAddressAsync(bluetoothaddress)
@@ -90,19 +126,20 @@ impl DeviceSearcher {
         let Ok(device) = device_future.await else {
             return None;
         };
-        let verifier: StatusVerifier = Box::new(|status| println!("{}", status));
-        let handler: BLStatusHandler = TypedEventHandler::new(
-            move |devopt: &Option<BluetoothLEDevice>, _args: &Option<IInspectable>| {
-                if let Some(device) = devopt {
-                    let connected: bool = device.ConnectionStatus().ok().map_or(false, |status| {
-                        status == BluetoothConnectionStatus::Connected
-                    });
-                    verifier(connected);
-                };
+        // let verifier: StatusVerifier = Box::new(|status| println!("{}", status));
+        // let handler: BLStatusHandler = TypedEventHandler::new(
+        //     move |devopt: &Option<BluetoothLEDevice>, _args: &Option<IInspectable>| {
+        //         if let Some(device) = devopt {
+        //             let connected: bool = device.ConnectionStatus().ok().map_or(false, |status| {
+        //                 status == BluetoothConnectionStatus::Connected
+        //             });
+        //             verifier(connected);
+        //         };
 
-                Ok(())
-            },
-        );
+        //         Ok(())
+        //     },
+        // );
+        let handler = TypedEventHandler::new(Self::INVOKE);
         let Ok(token) = device.ConnectionStatusChanged(&handler) else {
             return None;
         };
@@ -125,30 +162,30 @@ impl DeviceSearcher {
                     Some(signal) => {
                         println!("DeviceSearcher: {:?}", signal);
                         match signal {
-                            SearcherSignal::FindDevice(query) => match query {
-                                DeviceQuery::Address(bluetoothaddress) => {
-                                    let search_start = Instant::now();
-                                    'search: loop {
-                                        if search_start.elapsed() > timeout {
-                                            break 'search;
-                                        };
-                                        let Some(dw) =
+                            SearcherSignal::FindDevice(query) => {
+                                let search_start = Instant::now();
+                                'search: loop {
+                                    if search_start.elapsed() > timeout {
+                                        break 'search;
+                                    }
+                                    let Some(dw) = (match query {
+                                        DeviceQuery::Address(bluetoothaddress) => {
                                             Self::get_device_by_address(bluetoothaddress).await
-                                        else {
-                                            println!("nun");
-                                            continue;
-                                        };
-                                        match output_tx.send(dw) {
-                                            Ok(_) => println!("Sent"),
-                                            _ => {
-                                                println!("HELLO WTF");
-                                                break 'main;
-                                            }
+                                        }
+                                        DeviceQuery::Id(ref hs) => Self::get_device_by_id(hs).await,
+                                    }) else {
+                                        println!("nun");
+                                        continue 'search;
+                                    };
+                                    match output_tx.send(dw) {
+                                        Ok(_) => println!("Sent"),
+                                        _ => {
+                                            println!("HELLO WTF");
+                                            break 'main;
                                         }
                                     }
                                 }
-                                _ => todo!(),
-                            },
+                            }
                         }
                     }
                     None => {
